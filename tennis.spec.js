a9d9691b463cc6256ac0fa7043cbc6168b4b64cf
Sasithon <amfouy@gmail.com>
2017-09-09 12:50:28 +0700

    Pass4

--- tennis.spec.js
+++ tennis.spec.js
@@ -1,41 +1,76 @@
 function TennisGame() {
     const scoreString = ['Love', 'Fifteen', 'Thirty', 'Forthy']
     this.playerAScore = 0
+      this.playerBScore = 0
+
 
     this.reset = () => {
         this.playerAScore = 0
+        this.playerBScore = 0
     }
 
     this.echo = () => {
-       return `${scoreString[this.playerAScore]} - Love`
+       return `${scoreString[this.playerAScore]} - ${scoreString[this.playerBScore]}`
     }
 
-    this.playerAGteScore = () => {
+    this.playerAGetScore = () => {
         this.playerAScore++
     }
+
+    this.playerBGetScore = () => {
+        this.playerBScore++
+    }
 }
+describe('Echo()',() => {
+  let app
 
-test('Echo "Love - Love" when game start', () => {
-    // Arrange
-    let app = new TennisGame()
-
-    // Act
+  beforeEach(()=>{
+    app=new TennisGame()
     app.reset()
-    let result = app.echo()
+  })
+    test('Echo "Love - Love" when game start', () => {
+        // Arrange
 
-    // Assert
-    expect(result).toBe('Love - Love')
-})
+        let result = app.echo()
 
-test('Echo "Fifteen - Love" when playerA get first score', () => {
-    // Arrange
-    let app = new TennisGame()
-    app.reset()
-    app.playerAGteScore()
+        // Assert
+        expect(result).toBe('Love - Love')
+    })
 
-    // Act
-    let result = app.echo()
+    test('Echo "Fifteen - Love" when playerA get first score', () => {
+        // Arrange
 
-    // Assert
-    expect(result).toBe('Fifteen - Love')
-})
+        app.playerAGetScore()
+
+        // Act
+        let result = app.echo()
+
+        // Assert
+        expect(result).toBe('Fifteen - Love')
+    })
+    test('Echo "Love - Fifteen" when playerB get first score', () => {
+        // Arrange
+
+        app.playerBGetScore()
+
+        // Act
+        let result = app.echo()
+
+        // Assert
+        expect(result).toBe('Love - Fifteen')
+    })
+    test('Echo "Thirty - Fifteen" when score is 30 - 15 ', () => {
+        // Arrange
+
+        app.playerAGetScore()
+        app.playerBGetScore()
+        app.playerAGetScore()
+
+        // Act
+        let result = app.echo()
+
+        // Assert
+        expect(result).toBe('Thirty - Fifteen')
+    })
+
+  })
